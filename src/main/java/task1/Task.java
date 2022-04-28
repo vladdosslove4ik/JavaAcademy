ccfeb9855a0bf83af4f6e541f9139c4b3f0264dd
Vladyslav Nahurnik <vladnagura@gmail.com>
2022-04-28 23:48:16 +0200

    gitignore configured and created structure for task solving

---
+++ src/main/java/task1/Task.java
@@ -0,0 +1,25 @@
+package task1;
+
+import java.util.Scanner;
+
+public class Task {
+
+
+    /**
+    @TODO counting algorithm
+
+    */
+    public static int[] countLetters(String s) {
+        return null;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        System.out.println(input);
+    }
+}
