febbd609043533f208e429541c40d2c62d9b7898
Vladyslav Nahurnik <vladnagura@gmail.com>
2022-04-29 00:30:36 +0200

    Algorithm created

--- src/main/java/task1/Task.java
+++ src/main/java/task1/Task.java
@@ -1,25 +1,58 @@
 package task1;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Task {
 
+    private static final int CHAR_CODE_ALPHABET_BEGINNING = 97;
+    private static final int CHAR_CODE_ALPHABET_ENDING = 122;
 
     /**
-    @TODO counting algorithm
-
-    */
+     * Case-insensitive function which Counts have many times letter occurs in string and return int array
+     * with counts of every letter.
+     * English alphabet expected
+     * @param s the string to count letters in
+     * @return Array with count of letters a-z in indexes 0-25
+     */
     public static int[] countLetters(String s) {
-        return null;
+
+        s = s.toLowerCase();
+        int[] countLetters = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if(isSmallLetter(c)) {
+                countLetters[c - 97]++;
+            }
+        }
+        return countLetters;
     }
 
+    public static boolean isSmallLetter(char c) {
+        if (c >= CHAR_CODE_ALPHABET_BEGINNING && c <= CHAR_CODE_ALPHABET_ENDING) return true;
+        else return false;
+    }
 
+    public static String createReport(int[] counts) {
+        
+        String report = "";
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] != 0) {
+                report += (char) (i + CHAR_CODE_ALPHABET_BEGINNING) + " = " + counts[i] + "\n";
+            }
+
+        }
+        return report;
+    }
 
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
 
-        System.out.println(input);
+        System.out.println(createReport(countLetters(input)));
+
     }
 }
